029b454bdd4cbb305c04f50c8baa3e0d54c4d09f
Jaime Nufio <jaimeenufio@gmail.com>
2017-03-14 09:59:53 -0400

    Flirty Bot is complete

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+	consumer_key:		'081Wv18MMx4bxk84k7MlO3JP7',
+	consumer_secret:	'vryK3lFXzM1WnVanUsRXY4UJl57jpf2lMPRCSbF53IGyZI1NjG',
+	access_token:		'841410564098260994-jgA3wViObQkomZldYZ4G2H5WqNDGQJl',
+	access_token_secret: 	'07pFfN0hybF0rgMASeybyzYWmINnipjGlagF9TYXE7emM',	
+}
